29977b50639384acc2bc194f2fba38455da5c02f
sudhakodali06 <72756905+sudhakodali06@users.noreply.github.com>
2021-06-04 12:16:33 +0530

    Add files via upload

---
+++ Project_Template_C9_Background_Color_Changer-main/Project_Template_C9_Background_Color_Changer-main/sketch.js
@@ -0,0 +1,37 @@
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400);  
+
+  btn_red = createButton("RED");
+  btn_red.position(100,50);
+  btn_red.mousePressed(redBg);
+
+  btn_green = createButton("GREEN");
+  btn_green.position(250,50);
+  btn_green.mousePressed(greenBg);
+
+
+}
+
+function draw() {
+  background(r,g,b);
+}
+
+function redBg(){
+r = 255;
+g=0;
+b=0;
+}
+
+function greenBg(){
+r=0;
+g=255;
+b=0;
+}
+
